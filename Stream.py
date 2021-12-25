23038103f8bdbc580d128eaf9497a47dde541272
Dmitrii Kuzmin <cjxrust@gmail.com>
2021-12-25 12:37:29 +0700

    add ma and diff

--- Stream.py
+++ Stream.py
@@ -1,10 +1,17 @@
+import pandas as pd
+
 import streamlit as st
 import yfinance as yf
-
 from datetime import date
 from plotly import graph_objs as go
 
-start = "2015-01-01"
+import matplotlib as plt
+
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split
+
+
+start = "2010-01-01"
 today = date.today().strftime("%Y-%m-%d")
 
 st.title("Forecast Stock Price")
@@ -16,7 +23,7 @@ n_years = st.slider("Период предсказания: ", 1, 4)
 period = n_years * 365
 
 
-@st.cache
+# @st.cache
 def load_data(ticker):
     df = yf.download(ticker, start, today)
     df.reset_index(inplace=True)
@@ -31,12 +38,11 @@ data_load_state.text("Load ...done! ")
 st.header('Raw Date')
 st.write(df.tail())
 
-st.markdown('## Graph ')
-
 
 def plot_paint():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df['Date'], y=df['Close']))
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Close'], mode='markers', name='g(x)=x',
+                             marker=dict(color='LightSkyBlue', size=1.5, line=dict(color='Red', width=1))))
     fig.layout.update(title='Time Series Data', xaxis_rangeslider_visible=True)
 
     st.plotly_chart(fig)
@@ -44,4 +50,22 @@ def plot_paint():
 
 plot_paint()
 
-# st.line_chart(df['Close'])
+# Calculates Moving Average (5) base on stock  price.
+
+st.header('Calculates Moving Average (13) base on stock  price.')
+df['ma5'] = df['Close'].rolling(13).mean()
+st.line_chart(df['ma5'])
+
+# st.write(df.tail(3))
+
+train_data = df.iloc[5:2700, 7]
+
+# st.write(train_data)
+# st.line_chart(train_data)
+
+diff_data = train_data.diff()
+diff_data.dropna(inplace=True)
+
+st.header('Calculates the difference of a MA(13) dataframe previous element.')
+st.line_chart(diff_data)
+# Forecast
